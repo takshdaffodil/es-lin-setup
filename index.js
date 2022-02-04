702bcc82f63f6f7f8aba0716e00d166b9f4e38ac
takshmehta <takshmehta111@gmail.com>
2022-02-04 14:22:44 +0530

    print all json values

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-function getFullName({ firstName, lastName }) {
-  return `${firstName} ${lastName}`;
-}
+const message = require('./message.json');
 
-const nameObject = {
-  firstName: 'Taksh',
-  lastName: 'Mehta',
+// const messages = {
+//   glossary: {
+//     title: 'example glossary',
+//     GlossDiv: {
+//       title: 'S',
+//       GlossList: {
+//         GlossEntry: {
+//           ID: 'SGML',
+//           SortAs: 'SGML',
+//           GlossTerm: 'Standard Generalized Markup Language',
+//           Acronym: 'SGML',
+//           Abbrev: 'ISO 8879:1986',
+//           GlossDef: {
+//             para: 'A meta-markup language, used to create markup languages such as DocBook.',
+//             GlossSeeAlso: ['GML', 'XML'],
+//           },
+//           GlossSee: 'markup',
+//         },
+//       },
+//     },
+//   },
+// };
+// const newMessage = JSON.parse(JSON.stringify(messages));
+// newMessage.glossary.GlossDiv.GlossList.GlossEntry.GlossSee = 'markdown';
+
+// print all values of a json object
+
+const printAllValues = (jsonObject) => {
+  for (const key in jsonObject) {
+    if (typeof jsonObject[key] === 'object') {
+      printAllValues(jsonObject[key]);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`Key: ${key} : ${jsonObject[key]}`);
+    }
+  }
 };
 
-getFullName(nameObject);
-
-const message = {
-  glossary: {
-    title: 'example glossary',
-    GlossDiv: {
-      title: 'S',
-      GlossList: {
-        GlossEntry: {
-          ID: 'SGML',
-          SortAs: 'SGML',
-          GlossTerm: 'Standard Generalized Markup Language',
-          Acronym: 'SGML',
-          Abbrev: 'ISO 8879:1986',
-          GlossDef: {
-            para: 'A meta-markup language, used to create markup languages such as DocBook.',
-            GlossSeeAlso: ['GML', 'XML'],
-          },
-          GlossSee: 'markup',
-        },
-      },
-    },
-  },
-};
-const newMessage = JSON.parse(JSON.stringify(message));
-newMessage.glossary.GlossDiv.GlossList.GlossEntry.GlossSee = 'markdown';
+printAllValues(message);
