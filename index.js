9662db933bb94b2e4eae6d7d79f8606157de08a6
takshmehta <takshmehta111@gmail.com>
2022-02-04 13:04:56 +0530

    made deep copy of message object

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ const nameObject = {
 };
 
 getFullName(nameObject);
+
+const message = {
+  glossary: {
+    title: 'example glossary',
+    GlossDiv: {
+      title: 'S',
+      GlossList: {
+        GlossEntry: {
+          ID: 'SGML',
+          SortAs: 'SGML',
+          GlossTerm: 'Standard Generalized Markup Language',
+          Acronym: 'SGML',
+          Abbrev: 'ISO 8879:1986',
+          GlossDef: {
+            para: 'A meta-markup language, used to create markup languages such as DocBook.',
+            GlossSeeAlso: ['GML', 'XML'],
+          },
+          GlossSee: 'markup',
+        },
+      },
+    },
+  },
+};
+const newMessage = JSON.parse(JSON.stringify(message));
+newMessage.glossary.GlossDiv.GlossList.GlossEntry.GlossSee = 'markdown';
